dc41f185728cfd9742dd60c97a262c73bdad6df6
Embbnux Ji <embbnux@embbnux.com>
2022-08-08 17:53:47 +0800

    feat: use token in headers

--- frontend/lib/client.js
+++ frontend/lib/client.js
@@ -3,16 +3,27 @@ const TOKEN_STORAGE_KEY = 'rc-workflow-jwt-token';
 
 export class Client extends EventEmitter {
   async getUserInfo() {
-    const response = await fetch(`/users/me?token=${this.token}`);
+    const userInfo = await this._request('/users/me');
+    return userInfo;
+  }
+
+  async _request(path, method = 'GET', body = null) {
+    const response = await fetch(path, {
+      method,
+      headers: {
+        'x-access-token': this.token,
+        'Content-Type': 'application/json',
+      },
+      body: body ? JSON.stringify(body) : undefined,
+    });
     if (response.status === 401) {
       this.emit('unauthorized');
       throw new Error('Session expired!');
     }
     if (response.status !== 200) {
-      throw new Error('Fetch data error please retry later or reopen the dialog')
+      throw new Error('Fetch data error please retry later');
     }
-    const data = await response.json();
-    return data;
+    return response.json();
   }
 
   logout() {
