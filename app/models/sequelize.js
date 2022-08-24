3131eeb8573b847da9d80e0ac5e668915c259e5b
Embbnux Ji <embbnux@embbnux.com>
2022-08-24 22:01:09 +0800

    chore: add DB_SSL_UNSAFE flag

--- app/models/sequelize.js
+++ app/models/sequelize.js
@@ -1,5 +1,17 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+let option;
+if (process.env.DB_SSL_UNSAFE === 'true') {
+  option = {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  };
+}
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, option);
 
 exports.sequelize = sequelize;
